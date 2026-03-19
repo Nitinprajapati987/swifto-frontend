a2e234a4743d9376d81e2fab8752b7694f298950
Nitinprajapati987 <nitinprajapati.sh98@gmail.com>
2026-03-19 20:19:05 +0530

    Update Auth.js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { authAPI } from '../api';
+import { authAPI } from '../services/api';
 
 const CITIES = [
   'Indore','Pithampur','Bhopal','Dewas','Ujjain',
